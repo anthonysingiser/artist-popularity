99a8b4440b64b115fa7af7a9d5933d16f64e35c2
Anthony <asingiser@gmail.com>
2023-10-26 11:13:03 -0400

    add styling for active and inactive routes

--- app/components/NavMenu.jsx
+++ app/components/NavMenu.jsx
@@ -1,6 +1,10 @@
 "use client"
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
+import { usePathname } from "next/navigation"
+
+const ACTIVE_ROUTE = "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+const INACTIVE_ROUTE = "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 
 function AuthButton() {
     const { data: session } = useSession()
@@ -25,6 +29,7 @@ export default function NavMenu() {
     return (
         <div>
             <AuthButton />
+
         </div>
     )
 }
